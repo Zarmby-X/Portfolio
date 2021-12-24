4f4ea144f0de658fb38bff05855649ac7dd65a6c
ArmandoHR <Armando.Hernandez.r@hotmail.com>
2021-12-24 03:37:49 -0600

    nothing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Home from "./pages/home/Home";
 import Navbar from "./components/appConsts/navBar/Navbar";
-import { BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate, Link} from "react-router-dom";
 import Meme from "./pages/meme/Meme";
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
           <Route path="/Proyects" element={<Meme></Meme>} />
           <Route path="/Playground" element={<Meme></Meme>} />
           <Route path="/Contact" element={<Meme></Meme>} />
-          <Route path="/NotFound" element={<a href="https://zarmby-x.github.io/Portfolio/">error 404 no encontrado</a>} />
+          <Route path="/NotFound" element={<Link to="https://zarmby-x.github.io/Portfolio/">error 404 no encontrado</Link>} />
           <Route path="*" element={<Navigate to="/NotFound"/>}/>
         </Routes>
       </BrowserRouter>
