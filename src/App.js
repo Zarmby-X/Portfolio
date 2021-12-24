30fe4d6b79e5c28a7fce40a70e0b975e426e7003
ArmandoHR <Armando.Hernandez.r@hotmail.com>
2021-12-24 03:04:59 -0600

    nothin

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
 import Meme from "./pages/meme/Meme";
 
 function App() {
+
   return (
     <div>
       <BrowserRouter>
@@ -11,12 +12,13 @@ function App() {
         <Routes>
           <Route path="/" element={<Home></Home>}/>
           <Route path="/home" element={<Navigate to="/"/>}/>
+          <Route path="/Portfolio/" element={<Navigate to="/"/>}/>
           <Route path="/About" element={<Meme></Meme>} />
           <Route path="/Proyects" element={<Meme></Meme>} />
           <Route path="/Playground" element={<Meme></Meme>} />
           <Route path="/Contact" element={<Meme></Meme>} />
-          <Route path="/NotFound" element={<a href="http://localhost:3000">error 404 no encontrado</a>} />
-          <Route path="*" element={<Home></Home>} />
+          <Route path="/NotFound" element={<a href="https://zarmby-x.github.io/Portfolio/">error 404 no encontrado</a>} />
+          <Route path="*" element={<Navigate to="/NotFound"/>}/>
         </Routes>
       </BrowserRouter>
     </div>
