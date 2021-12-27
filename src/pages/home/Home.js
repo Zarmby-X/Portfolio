0238506cf699419ee054b177c1ec9a16bf174bef
ArmandoHR <Armando.Hernandez.r@hotmail.com>
2021-12-27 16:05:24 -0600

    se separaron los componentes de home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,64 +1,11 @@
-import React from "react";
 import "./Home.scss";
-import Profile from "../../img/Profile.jpg";
-import Fade from 'react-reveal/Fade';
-import { GitIcon, SassIcon, ReactIcon, JsIcon, NpmIcon, GitHubIcon } from "../../components/appConsts/icons/Icons"
-import { NavLink } from "react-router-dom";
+import Header from "./Header/Header"
 
 
 function Home() {
     return (
         <div id="homePage">
-            <header>
-                <div id="headerContent">
-                    <div id="leftSideHeader">
-                        <Fade bottom>
-                            <div>
-                                <p id="tittleIdPage1">{"Font-end Developer"}</p>
-                                <h1 id="tittleIdPage2">{"Armando Hernandez Rivera"}</h1>
-                                <div id="Tittle-Line"></div>
-                            </div>
-                        </Fade>
-                        <Fade bottom>
-                            <p id="headerDesc">Hi i'm Armando, i love design and create awesome web pages,
-                                I specialize in front-end development and i always want to learn new design techniques and technologies to improve my skills.</p>
-                            <NavLink to={"/About"}>
-                                <div id="headerButtonWrap">
-                                <button id="headerButton">About me!</button>
-                                </div>
-                            </NavLink>
-                        </Fade>
-                    </div>
-                    <div id="rightSideHeader">
-                        <Fade right>
-                            <div id="imgWrap">
-                                <img id="profileImg" alt="Armando Hernandez Rivera" src={Profile}></img>
-                            </div>
-                        </Fade>
-                    </div>
-                    <Fade bottom>
-                        <div id="headerBotton">
-                            <div id="headerBottonContent">
-                                <div id="leftLeftFooterSide">
-                                    <p>Core technologies...</p>
-                                </div>
-                                <div id="rigthLeftFooterSide">
-                                    <Fade>
-                                        <div id="headerFooterIconsWrap">
-                                            <JsIcon className={"navBarFooterIcon"}></JsIcon>
-                                            <SassIcon className={"navBarFooterIcon"}></SassIcon>
-                                            <ReactIcon className={"navBarFooterIcon"}></ReactIcon>
-                                            <GitHubIcon className={"navBarFooterIcon"}></GitHubIcon>
-                                            <NpmIcon className={"navBarFooterIcon"}></NpmIcon>
-                                            <GitIcon className={"navBarFooterIcon"}></GitIcon>
-                                        </div>
-                                    </Fade>
-                                </div>
-                            </div>
-                        </div>
-                    </Fade>
-                </div>
-            </header>
+            <Header></Header>
         </div>
     );
 }
--- src/pages/meme/Meme.js
+++ src/pages/meme/Meme.js
@@ -7,7 +7,7 @@ function Meme() {
             <p>Que hace aqui joven? esta pagina sigue en construccion</p>
             <p>Orale siga con lo suyo</p>
             <img src={cheems} alt="builderCheems"></img>
-            <a  style={{display: "table-cell"}} id="memebtn" href="https://www.youtube.com/watch?v=RRRtPoxr3dE" target = "_blank" rel="noreferrer">Regresar</a>
+            <a  style={{display: "table-cell"}} id="memebtn" href="https://www.youtube.com/watch?v=RRRtPoxr3dE" target = "_blank" rel = "noopener noreferrer">Regresar</a>
         </div>
     );
 }
